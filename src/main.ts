d9fbf50b824d3847fc0da5a3365e86c0479e06ad
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-03-23 05:03:53 +0900

    release: 0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,50 @@
-import { Plugin } from 'obsidian';
-import { MyPluginSettings, DEFAULT_SETTINGS, SampleSettingTab } from 'settings';
+import { Plugin, WorkspaceItem, WorkspaceSplit } from 'obsidian';
+import { ResizeSplitSettings, DEFAULT_SETTINGS, ResizeSplit } from 'settings';
 
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+declare module 'obsidian' {
+	interface Workspace {
+		requestResize(): void;
+	}
+
+	interface WorkspaceItem {
+		containerEl: HTMLElement;
+		parent?: WorkspaceParent;
+		dimension: number | null;
+		setDimension(dimension: number | null): void;
+	}
+
+	interface WorkspaceParent {
+		children: WorkspaceItem[];
+	}
+
+	interface WorkspaceSplit {
+		getElSize(el: HTMLElement): number | undefined;
+	}
+}
+
+
+export default class ResizeSplitPlugin extends Plugin {
+	settings: ResizeSplitSettings;
 
 	async onload() {
 		await this.loadSettings();
 		await this.saveSettings();
-		this.addSettingTab(new SampleSettingTab(this));
+		this.addSettingTab(new ResizeSplit(this));
+
+		this.addCommand({
+			id: 'resize-left',
+			name: 'Resize to left or top',
+			checkCallback: (checking) => this.resizeActiveSplit(checking, -1, this.settings.step),
+			repeatable: true
+		});
+
+		this.addCommand({
+			id: 'resize-right',
+			name: 'Resize to right or bottom',
+			checkCallback: (checking) => this.resizeActiveSplit(checking, +1, this.settings.step),
+			repeatable: true
+		});
 	}
 
 	async loadSettings() {
@@ -17,5 +53,43 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	resizeActiveSplit(checking: boolean, direction: 1 | -1, step: number) {
+		let split: WorkspaceItem = this.app.workspace.rootSplit;
+		while (split) {
+			if (!(split instanceof WorkspaceSplit)) return false;
+			if (split.children.length > 2) return false;
+			if (split.children.length === 2) break;
+			split = split.children[0];
+		}
+
+		const [leftChild, rightChild] = split.children;
+		const leftSize = split.getElSize(leftChild.containerEl);
+		const rightSize = split.getElSize(rightChild.containerEl);
+
+		if (typeof leftSize !== 'number' || typeof rightSize !== 'number') {
+			return false;
+		}
+
+		console.log(leftChild, rightChild)
+
+		if (!checking) {
+			const clip = (val: number) => Math.max(Math.min(val, 90), 10);
+			const leftDimension = clip(leftSize / (leftSize + rightSize) * 100);
+
+			const newLeftDimension = clip(Math.round((leftDimension + step * direction) / step) * step);
+			const newRightDimension = 100 - newLeftDimension;
+
+			console.log({ leftDimension, newLeftDimension })
+
+			leftChild.setDimension(newLeftDimension);
+			rightChild.setDimension(newRightDimension);
+
+			this.app.workspace.requestSaveLayout();
+			this.app.workspace.requestResize();
+		}
+
+		return true;
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,87 +1,24 @@
 import { PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from 'main';
+import ResizeSplitPlugin from 'main';
 
 
-export interface MyPluginSettings {
+export interface ResizeSplitSettings {
+	step: number;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
+export const DEFAULT_SETTINGS: ResizeSplitSettings = {
+	step: 5,
 };
 
 // Inspired by https://stackoverflow.com/a/50851710/13613783
 export type KeysOfType<Obj, Type> = NonNullable<{ [k in keyof Obj]: Obj[k] extends Type ? k : never }[keyof Obj]>;
 
-export class SampleSettingTab extends PluginSettingTab {
-	constructor(public plugin: MyPlugin) {
+export class ResizeSplit extends PluginSettingTab {
+	constructor(public plugin: ResizeSplitPlugin) {
 		super(plugin.app, plugin);
 	}
 
-	addHeading(heading: string) {
-	    return new Setting(this.containerEl).setName(heading).setHeading();
-    }
-
-	addTextSetting(settingName: KeysOfType<MyPluginSettings, string>) {
-		return new Setting(this.containerEl)
-			.addText((text) => {
-				text.setValue(this.plugin.settings[settingName])
-					.setPlaceholder(DEFAULT_SETTINGS[settingName])
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value;
-						await this.plugin.saveSettings();
-					});
-			});
-	}
-
-	addNumberSetting(settingName: KeysOfType<MyPluginSettings, number>) {
-		return new Setting(this.containerEl)
-			.addText((text) => {
-				text.setValue('' + this.plugin.settings[settingName])
-					.setPlaceholder('' + DEFAULT_SETTINGS[settingName])
-					.then((text) => text.inputEl.type = "number")
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value === '' ? DEFAULT_SETTINGS[settingName] : +value;
-						await this.plugin.saveSettings();
-					});
-			});
-	}
-
-	addToggleSetting(settingName: KeysOfType<MyPluginSettings, boolean>, extraOnChange?: (value: boolean) => void) {
-		return new Setting(this.containerEl)
-			.addToggle((toggle) => {
-				toggle.setValue(this.plugin.settings[settingName])
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value;
-						await this.plugin.saveSettings();
-						extraOnChange?.(value);
-					});
-			});
-	}
-
-	addDropdowenSetting(settingName: KeysOfType<MyPluginSettings, string>, options: readonly string[], display?: (option: string) => string, extraOnChange?: (value: string) => void) {
-		return new Setting(this.containerEl)
-			.addDropdown((dropdown) => {
-				const displayNames = new Set<string>();
-				for (const option of options) {
-					const displayName = display?.(option) ?? option;
-					if (!displayNames.has(displayName)) {
-						dropdown.addOption(option, displayName);
-						displayNames.add(displayName);
-					}
-				};
-				dropdown.setValue(this.plugin.settings[settingName])
-					.onChange(async (value) => {
-						// @ts-ignore
-						this.plugin.settings[settingName] = value;
-						await this.plugin.saveSettings();
-						extraOnChange?.(value);
-					});
-			});
-	}
-
-	addSliderSetting(settingName: KeysOfType<MyPluginSettings, number>, min: number, max: number, step: number) {
+	addSliderSetting(settingName: KeysOfType<ResizeSplitSettings, number>, min: number, max: number, step: number) {
 		return new Setting(this.containerEl)
 			.addSlider((slider) => {
 				slider.setLimits(min, max, step)
@@ -95,7 +32,10 @@ export class SampleSettingTab extends PluginSettingTab {
 			});
 	}
 	
-	display(): void {
+	display() {
 		this.containerEl.empty();
+
+		this.addSliderSetting('step', 1, 30, 1)
+			.setName('Resize step size (%)');
 	}
 }
